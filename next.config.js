209784bb5766ec45d5fa7b936c9f852acd1ca221
Blockchain Developer <92112210+SunTeco@users.noreply.github.com>
2022-09-24 11:32:34 +0300

    First commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  distDir: 'build',
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
+
+module.exports = nextConfig;
